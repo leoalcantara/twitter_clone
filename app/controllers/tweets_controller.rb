55ade519df6436f539d5ff0be3cae25f89244a7e
leoalcantara <leonidio.ti@gmail.com>
2018-08-18 18:10:25 -0300

    fim do primeiro dia

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,19 @@
 class TweetsController < ApplicationController
+
+	def create
+		@tweet =  current_user.tweets.new(tweet_params)
+
+		if @tweet.save
+			redirect_to root_path, notice:'Tweet enviado'
+
+		else
+			redirect_to root_path, notice:'Tweet inválido'				
+		end	
+
+	end
+
+	private
+	def tweet_params
+		params.require(:tweet).permit(:content)
+	end	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
 
-resources :tweets
+	resources :tweets, only: :create 
+	resources :users, only:[:index, :show]
 
 end
