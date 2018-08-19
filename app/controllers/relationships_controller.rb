29719bfdc88c0a3d0ae9a30c8bd6e65b1feea1d8
leoalcantara <leonidio.ti@gmail.com>
2018-08-19 15:06:10 -0300

    User follow, unfollow methods

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,3 +1,14 @@
 class RelationshipsController < ApplicationController
+
+	def create 
+		current_user.follow!(User.find(params["id"]))
+		
+	end
+
+	def destroy
+		if current_user.following?(User.find(params['id']))
+			current_user.unfollow!(User.find(params['id']))
+			
+	end
 		
 end
